f70de0c35a985f0afbf7b565a339281242642ece
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2021-12-30 21:39:46 -0500

    Implement intcpy function

--- pointers/p3.c
+++ pointers/p3.c
@@ -3,22 +3,54 @@
 char strA[80] = "A string to be used for demonstration purposes";
 char strB[80];
 
+char *my_strcpy(char *destination, char *source)
+{
+    char *p = destination;
+    while (*source != '\0')
+    {
+        *p++ = *source++;
+    }
+    *p = '\0';
+    return destination;
+}   
+
+void intcpy(const int *source, int *target, int n) {
+    for (int i=0; i<n; i++) {
+        *target++ = *source++;
+    }
+}
+
+
 int main(void)
 {
+    my_strcpy(strB, strA);
+    puts(strB);
 
-    char *pA;     /* a pointer to type character */
-    char *pB;     /* another pointer to type character */
-    puts(strA);   /* show string A */
-    pA = strA;    /* point pA at string A */
-    puts(pA);     /* show what pA is pointing to */
-    pB = strB;    /* point pB at string B */
-    putchar('\n');       /* move down one line on the screen */
-    while(*pA != '\0')   /* line A (see text) */
+
+    char *pA;     // a pointer to type character
+    char *pB;     // another pointer to type character 
+    puts(strA);   // show string A 
+    pA = strA;    // point pA at string A 
+    puts(pA);     // show what pA is pointing to 
+    pB = strB;    // point pB at string B 
+    putchar('\n');       // move down one line on the screen 
+    while(*pA != '\0')   // line A (see text) 
     {
-        *pB++ = *pA++;   /* line B (see text) */
+        *pB++ = *pA++;   // line B (see text)
     }
-    *pB = '\0';          /* line C (see text) */
-    puts(strB);          /* show strB on screen */
+    *pB = '\0';          // line C (see text) 
+    puts(strB);          // show strB on screen 
+
+    int a[10] = {1,2,3,4,5};
+    int b[10];
+
+    intcpy(a, b, 3);
+   
+    for (int i=0; i< (sizeof(b)/sizeof(b[0])); i++) {
+        printf("%i", b[i]);
+    }
+
     return 0;
 }
+
 
