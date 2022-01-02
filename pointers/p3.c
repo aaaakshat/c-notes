bacc7285817d5b10f2182df4bfb50c1ad2f5fc23
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2022-01-01 21:14:48 -0500

    Fix dumb memory bug

--- pointers/p3.c
+++ pointers/p3.c
@@ -14,39 +14,35 @@ char *my_strcpy(char *destination, char *source)
     return destination;
 }   
 
-void intcpy(const int *source, int *target, int n) {
-    printf("target");
-    int *target_ptr = target;
+void intcpy(int *source, int *target, int n) {
     for (int i=0; i<n; i++) {
-        *target_ptr++ = *source++;
-        printf("%i", *target_ptr);
+        *target++ = *source++;
     }
-    printf("\n");
 }
 
 
 int main(void)
 {
     my_strcpy(strB, strA);
-    puts(strB);
+//    puts(strB);
 
 
     char *pA;     // a pointer to type character
     char *pB;     // another pointer to type character 
-    puts(strA);   // show string A 
+//    puts(strA);   // show string A 
     pA = strA;    // point pA at string A 
-    puts(pA);     // show what pA is pointing to 
+ //   puts(pA);     // show what pA is pointing to 
     pB = strB;    // point pB at string B 
-    putchar('\n');       // move down one line on the screen 
+  //  putchar('\n');       // move down one line on the screen 
     while(*pA != '\0')   // line A (see text) 
     {
         *pB++ = *pA++;   // line B (see text)
     }
     *pB = '\0';          // line C (see text) 
-    puts(strB);          // show strB on screen 
+   // puts(strB);          // show strB on screen 
 
     int a[10] = {1,2,3,4,5};
-    int b[10];
+    int b[10] = { };
 
     intcpy(a, b, 3);
 
@@ -54,11 +50,11 @@ int main(void)
     int *a_ptr = a;
    
     for (int i=0; i < 10; i++) {
-        printf("%i\n", *a_ptr++);
+        printf("%i", *a_ptr++);
     }
     printf("\n");
     for (int i=0; i < 10; i++) {
-        printf("%i\n", *b_ptr++);
+        printf("%i", *b_ptr++);
     }
     printf("\n");
 
