32a277a7d3193658631af944c52c316c50b97890
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2022-02-19 17:15:10 -0500

    Test strcmp

--- char_arrays.c
+++ char_arrays.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char name[4] = "bob";
     char empty[4] = "";
     char *n = name;
+    char *str = name;
 
     int a = *empty == '\0';
     int b = *empty == '\n';
     int c = *n == *name;
-    int d = *n == 'b';
+    int d = strcmp(str, "bob");
 
     printf("a: %d b: %d c: %d d: %d\n", a, b, c, d);
+    printf("%s\n", str);
 
     return 0;
 }
